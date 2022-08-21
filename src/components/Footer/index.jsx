783a39ebb56d2a681827680430b5d13ef6583e4e
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2022-08-21 22:48:09 +0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -30,7 +30,7 @@ export default () => {
                     <ul>
                         <h2>{ lang.footer.useful }</h2>
                         <li><a href="https://www.linkedin.com/in/joseph-sultan/">{ lang.footer.contact }</a></li>
-                        <li><a href="https://www.linkedin.com/in/joseph-sultan/">{ lang.footer.support UA }</a></li>
+                        <li><a href="https://www.linkedin.com/in/joseph-sultan/">{ lang.footer.support }</a></li>
                     </ul>
                     <ul>
                         <h2>{ lang.footer.navigate }</h2>
